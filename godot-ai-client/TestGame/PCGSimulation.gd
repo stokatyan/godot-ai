df803014a444d64505740a26a7534445c98ce43d
Shant Tokatyan <tokat.shant@gmail.com>
2024-11-01 10:10:16 -0700

    factor out simulation; normalize state representation

---
+++ godot-ai-client/TestGame/PCGSimulation.gd
@@ -0,0 +1,32 @@
+extends RefCounted
+
+class_name PCGSimulation
+
+var hero_position: Vector2
+var coin_position: Vector2
+
+var _map_size = Vector2(1500.0, 800.0)
+
+
+func new_game():
+	hero_position = Vector2(randf_range(-_map_size.x/2, _map_size.x/2), randf_range(-_map_size.y/2, _map_size.y/2))
+	hero_position = Vector2(randf_range(-_map_size.x/2, _map_size.x/2), randf_range(-_map_size.y/2, _map_size.y/2))
+	if is_game_complete():
+		new_game()
+
+func is_game_complete() -> bool:
+	return hero_position.distance_to(coin_position) < 80
+
+func move_hero(force: float, direction: float, callback = null):
+	var move_vector = Vector2.from_angle(direction)
+	hero_position += move_vector * force * 10
+	if callback:
+		callback.call(self)
+
+func get_game_state() -> Array[float]:
+	var x_r = _map_size.x/2
+	var y_r = _map_size.y/2
+	return [hero_position.x/x_r, hero_position.y/y_r, coin_position.x/x_r, coin_position.y/y_r]
+
+func get_score() -> float:
+	return 1.0 - hero_position.distance_to(coin_position)
--- godot-ai-client/TestGame/PickupCoinGame.gd
+++ godot-ai-client/TestGame/PickupCoinGame.gd
@@ -11,9 +11,12 @@ var move_speed = 500
 var _is_loop_training = false
 var _is_testing = false
 
+var _simulation = PCGSimulation.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_new_game()
+	_simulation.new_game()
+	_display_simulation(_simulation)
 
 func _input(event):
 	var key_input = event as InputEventKey
@@ -31,15 +34,16 @@ func _input(event):
 			_is_testing = false
 
 		KEY_N:
-			_new_game()
+			_simulation.new_game()
+			_display_simulation(_simulation)
 		KEY_UP:
 			_setup_ai()
 		KEY_1: # Get and Apply action
-			var current_state = _get_game_state()
+			var current_state = _simulation.get_game_state()
 			var start_time = Time.get_ticks_msec()
 			var action = await _ai_tcp.get_action(current_state)
 			var _time_elapsed = (Time.get_ticks_msec() - start_time) / 1000.0
-			_move_hero(_time_elapsed, action[0])
+			_simulation.move_hero(action[1], action[0], _display_simulation)
 		KEY_2: # Get and Submit batch
 			var replays = await _get_batch_from_playing_round(false)
 			var response = await _ai_tcp.submit_batch_replay(replays)
@@ -64,59 +68,36 @@ func _physics_process(delta):
 		move_vector += Vector2.RIGHT
 
 	if apply_move:
-		_move_hero(delta, move_vector.angle())
-		if _is_game_complete():
-			_new_game()
+		_simulation.move_hero(1.0, move_vector.angle(), _display_simulation)
+		if _simulation.is_game_complete():
+			_simulation.new_game()
 			return
 
 func _setup_ai():
 	var result = _ai_tcp.attempt_connection_to_ai_server()
 	if !result:
 		return
-	result = await _ai_tcp.init_agent(4, 1, 20, 40)
+	result = await _ai_tcp.init_agent(4, 2, 256, 40)
 	_ai_tcp.load_agent(1)
 
-func _new_game():
-	hero.position = Vector2(randf_range(-_map_size.x/2, _map_size.x/2), randf_range(-_map_size.y/2, _map_size.y/2))
-	coin.position = Vector2(randf_range(-_map_size.x/2, _map_size.x/2), randf_range(-_map_size.y/2, _map_size.y/2))
-	if _is_game_complete():
-		_new_game()
-
-func _is_game_complete() -> bool:
-	return hero.position.distance_to(coin.position) < 80
-
-func _move_hero(delta_time: float, direction: float):
-	var move_vector = Vector2.from_angle(direction)
-	hero.position += move_vector * move_speed * delta_time
-
-func _get_game_state() -> Array[float]:
-	return [hero.position.x, hero.position.y, coin.position.x, coin.position.y]
-
-
 func _get_batch_from_playing_round(deterministic: bool) -> Array[Replay]:
-	_new_game()
+	_simulation.new_game()
 	var batch: Array[Replay] = []
 	var start_time = Time.get_ticks_msec()
 	var average_reward = 0
 
 	while Time.get_ticks_msec() - start_time < 1000.0:
-		var score_before = _get_score()
-		var t = Time.get_ticks_msec()
-		var state = _get_game_state()
+		var score_before = _simulation.get_score()
+		var state = _simulation.get_game_state()
 		var actions = await _ai_tcp.get_batch_actions([state], deterministic)
 		var action = actions[0] # Since we are not simulating in parallel, there is only 1 action
-		var _time_elapsed = (Time.get_ticks_msec() - t) / 1000.0
-		_move_hero(_time_elapsed, action[0])
-		var state_ = _get_game_state()
-		var is_done = _is_game_complete()
-		var score_after = _get_score()
+		_simulation.move_hero(action[1], action[0], _display_simulation)
+		var state_ = _simulation.get_game_state()
+		var is_done = _simulation.is_game_complete()
+		var score_after = _simulation.get_score()
 		var reward = score_after - score_before
-		if reward > 0:
-			reward = 100
-		else:
-			reward = -100
-		if is_done:
-			reward += 200
+		#if is_done:
+			#reward += 50
 		var replay = Replay.new(state, action, reward, state_, is_done)
 		batch.append(replay)
 		average_reward = reward
@@ -129,9 +110,6 @@ func _get_batch_from_playing_round(deterministic: bool) -> Array[Replay]:
 		print("Average Reward: " + str(average_reward))
 
 	return batch
-
-func _get_score() -> float:
-	return 1000.0 - hero.position.distance_to(coin.position)
 
 func _loop_train():
 	if _is_loop_training:
@@ -147,3 +125,7 @@ func _loop_train():
 		return
 	else:
 		_loop_train()
+
+func _display_simulation(sim: PCGSimulation):
+	hero.position = sim.hero_position
+	coin.position = sim.coin_position
