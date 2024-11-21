5db6f553444fbe7573122ef2721af417a0e4a9cc
Shant Tokatyan <tokat.shant@gmail.com>
2024-11-21 15:50:53 -0800

    reduce sim count

--- godot-ai-client/FindCoinGame/FCGEnv.gd
+++ godot-ai-client/FindCoinGame/FCGEnv.gd
@@ -55,7 +55,7 @@ func new_simulation() -> BaseSimulation:
 	return FCGSimulation.new() as FCGSimulation
 
 func get_simulation_count() -> int:
-	return 40
+	return 30
 
 func get_steps_in_round() -> int:
 	return 36
