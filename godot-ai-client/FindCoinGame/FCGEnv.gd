5bc0e0e15324162984fb5060bce880955dc73627
Shant Tokatyan <tokat.shant@gmail.com>
2024-12-05 23:31:05 -0800

    rebalance scoring

--- godot-ai-client/FindCoinGame/FCGEnv.gd
+++ godot-ai-client/FindCoinGame/FCGEnv.gd
@@ -87,10 +87,10 @@ func new_simulation() -> BaseSimulation:
 	return FCGSimulation.new() as FCGSimulation
 
 func get_simulation_count() -> int:
-	return 45
+	return 50
 
 func get_steps_in_round() -> int:
-	return 60
+	return 55
 
 func get_state_dim(_agent_name: String) -> int:
 	if _agent_name == _example_sim.get_agent_names()[0]:
