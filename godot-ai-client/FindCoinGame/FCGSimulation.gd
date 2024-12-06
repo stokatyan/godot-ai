9260004a4c5e3fe7de335bd30a78a51ac5bd7cd9
Shant Tokatyan <tokat.shant@gmail.com>
2024-12-06 10:01:02 -0800

    revert scoring

--- godot-ai-client/FindCoinGame/FCGSimulation.gd
+++ godot-ai-client/FindCoinGame/FCGSimulation.gd
@@ -309,12 +309,12 @@ func _get_collision_points(shape_rid: RID, transform: Transform2D, margin: float
 func get_score(agent_index: int) -> float:
 	if agent_index == 0:
 		if is_game_complete(agent_index):
-			return 50.0
-		return -10.0
+			return 1.0
+		return -1.0
 	else:
 		if is_game_complete(agent_index):
-			return -10.0
-		return 10.0
+			return -1.0
+		return 1.0
 
 ## Check if p2 will overlap the line from p1 to p3
 func _will_overlap(p1: Vector2, p3: Vector2, p2: Vector2, r: float):
