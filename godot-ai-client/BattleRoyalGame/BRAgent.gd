0164a11367e6d01209ddf4574590e63549a3eaa9
Shant Tokatyan <tokat.shant@gmail.com>
2024-12-15 13:54:47 -0800

    Working on bio stats for BR agnt

--- godot-ai-client/BattleRoyalGame/BRAgent.gd
+++ godot-ai-client/BattleRoyalGame/BRAgent.gd
@@ -5,6 +5,24 @@ class_name BRAgent
 var _health: float = 1.0
 var _attack_damage: float = 0.25
 
+var _current_ammo: float = 0.20
+var _ammo_per_reload: float = 0.20
+var _ammo_cost_per_shot: float:
+	get:
+		return 0.01
+
+var _reload_delay_remaining = 0.0
+var _reload_speed = 0.1
+var _reload_time_per_frame: float:
+	get:
+		return 0.01
+
+var _fire_rate = 0.5
+var _fire_delay_remaining = 0.0
+var _fire_time_per_frame: float:
+	get:
+		return 0.01
+
 var max_vision_distance: float:
 	get:
 		return 700
@@ -44,3 +62,16 @@ func get_vision_angles() -> Array[float]:
 func did_get_hit(damage_taken: float):
 	_health -= damage_taken
 	_health = max(0, _health)
+
+## Get all agent stats converted from 0->1 to -1->1
+func get_stats() -> Array[float]:
+	var stats: Array[float] = [
+		_health * 2.0 - 1.0,
+		_attack_damage * 2.0 - 1.0,
+		_current_ammo * 2.0 - 1.0,
+		_ammo_per_reload * 2.0 - 1.0,
+		_reload_delay_remaining * 2.0 - 1.0,
+		_reload_speed * 2.0 - 1.0,
+	]
+
+	return stats
